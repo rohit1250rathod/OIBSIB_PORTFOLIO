ee9a2e7bd8268cb4adf0416f0bb4e95b2865cf65
rohit1250rathod <rohitrathodse2024@gmail.com>
2023-06-24 11:00:35 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,39 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+let btn = document.getElementById("para");
+let btnknow = document.getElementById("btn");
+function showpara() {
+  btn.classList.toggle("aactive");
+  let bttn = document.querySelector(".aactive");
+  if (bttn) {
+    btnknow.innerText = `Read less`;
+  } else {
+    btnknow.innerText = `Read more`;
+  }
+}
